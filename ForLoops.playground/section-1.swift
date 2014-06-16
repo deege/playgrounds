c402787e5e7591641c58164809ad7727bb527622
Firel33t!! <dj@deege.com>
2014-06-16 11:01:45 -0600

    Added functions playground

--- ForLoops.playground/section-1.swift
+++ ForLoops.playground/section-1.swift
@@ -1,23 +1,21 @@
-// Playground - noun: a place where people can play
+/* This playground enumerates the different types of for loops */
 
 import Cocoa
 
-var str = "Hello, playground"
-
 // 1 to 3 not including 3
 for i in 1..3 {
-    println(str + "\(i)")
+    println("Looping \(i)")
 }
 
 // 1 to 3 including 3
 for i in 1...3 {
-    println(str + "\(i)")
+    println("Looping again \(i)")
 }
 
 // 1 to 5 in a list of numbers
 let i = 1..5
 for j in i {
-    println(str + "\(j)")
+    println("Looping over a list of numbers \(j)")
     if j == 3 {
         break
     }
@@ -25,14 +23,19 @@ for j in i {
 
 // Old school for-loop
 for var i=0; i<5; i++ {
-    println(str + "\(i)")
+    println("Old school looping \(i)")
 }
 
 // iterate over array of strings
 let s = ["Larry", "Curly", "Moe"]
 for h in s {
-    println(h)
+    println("Looping in an array of strings " + h)
 }
+
+//s.map(println)
+
+//var numbers = [1, 3, 5, 7]
+//numbers.map(println)
 
 // iterate over an array of strings with indexes
 for (index, value) in enumerate(s) {
@@ -47,7 +50,7 @@ for (game, company) in games {
 
 // Simple for-loop without using a index
 for _ in 1...6 {
-    println("hi there")
+    println("Looping without an index")
 }
 
 // Iterate over characters in a string
@@ -58,13 +61,13 @@ for character in "Albuquerque" {
 // while loop
 var index = 0
 while index < 10 {
-    println("in while loop")
+    println("Looping in a while loop")
     index++
 }
 
 // do while loop
 index = 0
 do {
-    println("in do while loop")
+    println("Looping in a do while loop")
     index++
 } while index < 5
--- Functions.playground/section-1.swift
+++ Functions.playground/section-1.swift
@@ -1,5 +1,96 @@
-// Playground - noun: a place where people can play
+// This playground is still under construction
 
 import Cocoa
 
 var str = "Hello, playground"
+
+// Function takes and returns a double
+func square(a:Int) -> Int {
+    return a * a
+}
+
+func odd(a:Int) -> Bool {
+    return ((a % 2) == 0)
+}
+
+func toAString(a:Int, b:Int) -> String {
+    return "\(a) - \(b)"
+}
+
+// Two return values
+func funtionWithTwoReturnValues() ->(first:String, second:Int) {
+    return ("Hello", 3);  // always returns hello and 3
+}
+
+// Call it once
+square(9)
+toAString(1, 2)
+
+// Variadic Paramerters
+func variadicFunction(strings:String...) {
+    // Using a for-loop because closures aren't working right
+    var returnString = ""
+    for s in strings {
+        returnString += s
+    }
+}
+variadicFunction("Hello")
+variadicFunction("Hello", "World", "!")
+
+// Force the name to be used for all parameters externally
+func requiredParamName(extName aName: String) -> String {
+    return aName;
+}
+requiredParamName(extName: "test");
+
+// Force name, but use the locally defined name
+func requiredParamName2(#aName: String) -> String {
+    return aName;
+}
+requiredParamName2(aName: "test");
+
+// Default parameters in function
+func defaultParamFunction(aName:String = "Test") -> String {
+    return aName;
+}
+defaultParamFunction()
+defaultParamFunction(aName: "Different")
+
+
+
+// call it as a closure
+var numbers: Array<Int> = [1,2,3,4,5,6,7,8,9]
+numbers.map(square)
+numbers.map({$0 * $0})
+let describedArray = numbers.map { "Number: \($0)" }
+
+// Call it as a filter
+numbers.filter(odd)
+numbers.filter({$0 % 2 == 0})
+
+// Call it in a reduction
+numbers.reduce(0, + )
+var reversed = sort(numbers, { $0 > $1 } )
+reversed = sort(numbers, > )
+reversed
+
+// Variable parameter for function
+var testString = "Testing"
+func varParameterFunction(var variable:String) {
+    variable = "New String"  // Can change the parameter value.  Can't do this without var
+    println(variable)
+}
+varParameterFunction(testString)  // Will always print New String
+
+// in and out parameter function
+func inoutParameterFunction(inout inoutString:String) {
+    inoutString = "Different String"
+    println(inoutString)
+}
+inoutParameterFunction(&testString)  // The & is required to use the variable after passing it
+
+
+
+
+
+
